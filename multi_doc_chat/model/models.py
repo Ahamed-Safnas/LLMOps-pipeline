ede155dd2659091d613b855768c65c50d379fe8a
safnasNED25 <safnas2042ahamed@gmail.com>
2026-05-17 23:11:36 +0530

    model validation schema

---
+++ multi_doc_chat/model/models.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class PromptType(str, Enum):
+    CONTEXTUALIZE_QUESTION = "contextualize_question"
+    CONTEXT_QA = "context_qa"
+
+
+class ChatAnswer(BaseModel):
+    answer: str = Field(min_length=1)
